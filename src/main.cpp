ec78c4711c63bfe3c4951abd7c005de090acc611
thatscringebro <thatscringebro@tutanota.com>
2023-11-28 21:00:54 -0500

    Début du tp, thank you gepeto

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,24 @@
 #include <Arduino.h>
 #include <map>
 #include <ESP8266WiFi.h>
-#include <ESP8266WebServer.h>
 #include <PID_v1.h>
+#include <PubSubClient.h>
 
 double Setpoint, Input, Output;
 
-double aggKp=37, aggKi=0.5, aggKd=2;
-double consKp=37, consKi=0.5, consKd=2;
+double aggKp = 37, aggKi = 0.5, aggKd = 2;
+double consKp = 37, consKi = 0.5, consKd = 2;
 
 PID myPID(&Input, &Output, &Setpoint, consKp, consKi, consKd, DIRECT);
 
-ESP8266WebServer httpd(80);
+const char* mqttServer = "192.168.1.252";
+const int mqttPort = 1883;
+const char* mqttUser = "pi";
+const char* mqttPassword = "pi";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
 bool MijoteuseOn = false;
 int tempsInital = 0;
 unsigned int compteur = 0;
@@ -27,21 +34,19 @@ long tempsChauffage;
 long tempsMaxChauffage; 
 long tempsDebutChauffage;
 
-
 std::map<int, double> arrTemp5min;
 std::map<int, double> arrTemp2min;
-
 
 double getCurrentTemp() {
   // Read temperature from the sensor and return the value
   int valeur = analogRead(A0);
-  
+
   //Formule pour transformer en degré -> 10000 pour resistance 10k, 3977 pour beta
   double voltage = valeur * 5.0 / 1023.0;
   double resistance = 10000.0 * voltage / (5.0 - voltage);
-  double tempKelvin = 1.0 / (1.0 / 298.15 + log(resistance / 10000.0) / 3977); 
+  double tempKelvin = 1.0 / (1.0 / 298.15 + log(resistance / 10000.0) / 3977);
 
-  double tempCelcius = tempKelvin - 273.15; 
+  double tempCelcius = tempKelvin - 273.15;
 
   return tempCelcius;
 }
@@ -103,7 +108,6 @@ void setTemperature(double Temperature){
   tempsDebut = tempsCourant;
 }
 
-
 String getIntensite() {
   return String((Output * 100) / 255);
 }
@@ -125,89 +129,71 @@ String getTemps() {
   return hrMinSec;
 }
 
-void handleRacine(){
-  String response = "<html><body>C'EST TROP COOL</body></html>";
-  httpd.send(200, "text/html", response);
-}
+void callback(char* topic, byte* payload, unsigned int length) {
+  // Fonction appelée lorsqu'un message est reçu sur un topic auquel nous sommes abonnés
 
-void handleMijoteuse() {
-  if (httpd.hasArg("action"))
-  {
-    String action = httpd.arg("action");
-    if(action == "on")
+  // Convertir le payload en une chaîne de caractères
+  payload[length] = '\0'; // Assurez-vous de terminer la chaîne
+  String message = String((char*)payload);
+
+  // Vérifier le topic sur lequel le message a été reçu
+  if (String(topic) == "yogourt/control") {
+    // Gérer le message reçu sur le topic "yogourt/control"
+    if (message == "on") {
+      // Recommencer à chauffer
       MijoteuseOn = true;
-    else if (action == "off")
+    } else if (message == "off") {
+      // Arrêter de chauffer
       MijoteuseOn = false;
+    }
   }
-  
-  String reponse = "<html><body>";
-    reponse += "<p>Temperature courante: " + String(getCurrentTemp(), 2) + "</p>";
-    reponse += "<p>Temperature 2min: " + String(min2Min, 2) + " - " + String(max2Min) + "</p>";
-    reponse += "<p>Temperature 5min: " + String(min5Min, 2) + " - " + String(max5Min, 2) + "</p>";
-    reponse += "<p>Temps de temperature: " + getTemps() + "</p>";
-    reponse += "<p>Intensite de chauffage: " + getIntensite() + "%</p>";
-    reponse += "<a href=\"/mijoteuse?action=on\">ON</a>";
-    reponse += "<a href=\"/mijoteuse?action=off\">OFF</a>";
-    reponse += "<div style=\"width: 10px; height: 10px; background-color: ";
-    reponse += (MijoteuseOn ? "green" : "red");
-    reponse += "\"></div>";
-    reponse += "</body></html>";
-
-    httpd.send(200, "text/html", reponse.c_str()); 
 }
 
 void setup() {
   Serial.begin(115200);
 
   Input = analogRead(A0);
-  Setpoint =  43.00;
+  Setpoint = 43.00;
   myPID.SetMode(AUTOMATIC);
-  pinMode(D1, OUTPUT);
 
-  Serial.println("Creation de l'AP...");
-  WiFi.softAP("WifiTropCool", "Qwerty123!");
-  Serial.println(WiFi.softAPIP());
+  Serial.println("Connecting to WiFi...");
+  WiFi.begin("DEPTI_2.4", "2021depTI");
 
-  httpd.on("/", handleRacine);
-  httpd.on("/mijoteuse", handleMijoteuse);
+  // Attendre la connexion au réseau
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
 
-  httpd.begin();
+  Serial.println("Connected to WiFi");
 
+  client.setServer(mqttServer, mqttPort);
+  if (client.connect("ESP8266_Client", mqttUser, mqttPassword)) {
+    Serial.println("Connected to MQTT broker");
+  } else {
+    Serial.println("Connection to MQTT broker failed");
+  }
 }
 
 void loop() {
+  client.loop();
+
   long currentTime = millis();
   elapsedTime = currentTime - startTime;
-  double temp = 0;
-  if (elapsedTime >= 100)
-  {
-    Serial.println("chauffing");
-    temp = getCurrentTemp();
-    Input = temp;
-    setTemperature(temp);
-    Serial.println("température min (2minutes) : " + String(min2Min, 2));
-    Serial.println("température max (2minutes) : " + String(max2Min, 2));
-    Serial.println("température min (5minutes) : " + String(min5Min, 2));
-    Serial.println("température max (5minutes) : " + String(max5Min, 2));
+  double temp = getCurrentTemp();
+  Input = temp;
+  setTemperature(temp);
 
-  if(temp < 41 || temp > 45){
-    tempsInital = 0;
-    Serial.println("temps initiale: " + String(tempsInital));
-    Serial.println("temperature: " + String(temp));
-  }
-  else if(tempsInital == 0)
-    tempsInital = millis();
-  }
-
-  if(temp <= 43) {
+  if (temp <= 43) {
     double gap = abs(Setpoint - Input);
-   if(gap < 10)
+    if (gap < 10)
       myPID.SetTunings(consKp, consKi, consKd);
     else
       myPID.SetTunings(aggKp, aggKi, aggKd);
-   myPID.Compute();
+    myPID.Compute();
   }
-  if(elapsedTime >= 100)
+
+  if (elapsedTime >= 100)
     startTime = currentTime;
 
   unsigned long tempsActuel = millis();
@@ -222,5 +208,4 @@ void loop() {
       tempsDebutChauffage = millis();
     }
   }
-  httpd.handleClient();
 }
